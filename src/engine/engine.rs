cba43b36794ceaf4e447a8375b5d12ea144abc7b
Guilherme <guilhermebentodasilva55@gmail.com>
2026-03-15 01:38:45 -0300

    refact lua function

--- src/engine/engine.rs
+++ src/engine/engine.rs
@@ -1,13 +1,11 @@
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use tokio::fs;
 use mlua::prelude::*;
 use tracing::error;
-use std::collections::HashMap;
 
 use super::models::lua_input::LuaInput;
-use super::models::lua_output::LuaOutput;
 
-use crate::engine::{rules::find_rule_by_agent, storage::{read_db, write_db}};
+use crate::engine::{lua_api::{convert_to_proto, inject_redb_api, make_idle, run_script}, rules::find_rule_by_agent};
 #[allow(non_snake_case)]
 use crate::g_rpc::g_rpc::cardinal_core::{Pulse, Reaction};
 
@@ -29,7 +27,7 @@ impl RuleEngine {
 
         let mut dir_entries = match fs::read_dir(&target_dir).await {
             Ok(entries) => entries,
-            Err(_) => return Self::make_idle("DIR_NOT_FOUND"),
+            Err(_) => return make_idle("DIR_NOT_FOUND"),
         };
 
        let mut scripts: Vec<(PathBuf, String)> = Vec::new();
@@ -45,7 +43,7 @@ impl RuleEngine {
         
         let lua = Lua::new();
 
-        if let Err(e) = Self::inject_redb_api(&lua).await {
+        if let Err(e) = inject_redb_api(&lua).await {
             error!("Redb inject error {}", e);
         }
         
@@ -54,16 +52,16 @@ impl RuleEngine {
             telemetry: &pulse.telemetry 
         };
         
-        let mut best_reaction = Self::make_idle("NO_ACTION");
+        let mut best_reaction = make_idle("NO_ACTION");
         let mut max_priority = -1;
 
         for (path, script_content) in scripts {
-            match Self::run_script(&lua, &script_content, &input).await {
+            match run_script(&lua, &script_content, &input).await {
                 Ok(output) => {
                     let priority = output.priority.unwrap_or(0);
                     if priority > max_priority {
                         max_priority = priority;
-                        best_reaction = Self::convert_to_proto(output);
+                        best_reaction = convert_to_proto(output);
                         if priority >= 1000 { break; }
                     }
                 },
@@ -74,61 +72,61 @@ impl RuleEngine {
         best_reaction
     }
 
-    async fn run_script(lua: &Lua, script_content: &str, input: &LuaInput<'_>) -> LuaResult<LuaOutput> {
-        let input_value = lua.to_value(input)?;
+    // async fn run_script(lua: &Lua, script_content: &str, input: &LuaInput<'_>) -> LuaResult<LuaOutput> {
+    //     let input_value = lua.to_value(input)?;
         
-        let globals = lua.globals();
-        globals.set("pulse", input_value)?;
+    //     let globals = lua.globals();
+    //     globals.set("pulse", input_value)?;
         
-        let value: mlua::Value = lua.load(script_content).eval_async().await?;
+    //     let value: mlua::Value = lua.load(script_content).eval_async().await?;
         
-        if let mlua::Value::Nil = value {
-            return Err(mlua::Error::RuntimeError("No action".into()));
-        }
+    //     if let mlua::Value::Nil = value {
+    //         return Err(mlua::Error::RuntimeError("No action".into()));
+    //     }
         
-        let output: LuaOutput = lua.from_value(value)?;
+    //     let output: LuaOutput = lua.from_value(value)?;
         
-        Ok(output)
-    }
+    //     Ok(output)
+    // }
 
-    fn convert_to_proto(out: LuaOutput) -> Reaction {
-        let type_enum = match out.action.as_str() {
-            "SHUTDOWN" => 1,
-            "RESTART" => 2,
-            "CUSTOM" => 3,
-            _ => 0,
-        };
-        Reaction {
-            trace_id: "multi-script".to_string(),
-            r#type: type_enum,
-            command_name: out.cmd_name.unwrap_or_default(),
-            parameters: out.params.unwrap_or_default(),
-        }
-    }
+    // fn convert_to_proto(out: LuaOutput) -> Reaction {
+    //     let type_enum = match out.action.as_str() {
+    //         "SHUTDOWN" => 1,
+    //         "RESTART" => 2,
+    //         "CUSTOM" => 3,
+    //         _ => 0,
+    //     };
+    //     Reaction {
+    //         trace_id: "multi-script".to_string(),
+    //         r#type: type_enum,
+    //         command_name: out.cmd_name.unwrap_or_default(),
+    //         parameters: out.params.unwrap_or_default(),
+    //     }
+    // }
 
-    fn make_idle(msg: &str) -> Reaction {
-        Reaction {
-            trace_id: "idle".to_string(),
-            r#type: 0,
-            command_name: msg.to_string(),
-            parameters: HashMap::new(),
-        }
-    }
+    // fn make_idle(msg: &str) -> Reaction {
+    //     Reaction {
+    //         trace_id: "idle".to_string(),
+    //         r#type: 0,
+    //         command_name: msg.to_string(),
+    //         parameters: HashMap::new(),
+    //     }
+    // }
 
-    async fn inject_redb_api(lua: &Lua) -> LuaResult<()> {
-        let redb_api = lua.create_table()?;
+    // async fn inject_redb_api(lua: &Lua) -> LuaResult<()> {
+    //     let redb_api = lua.create_table()?;
 
-        redb_api.set("set", lua.create_async_function(|_, (key, value): (String, u64)| async move {
-            write_db(key.as_str(), value).await.map_err(mlua::Error::external)?;
-            Ok(())
-        })?)?;
+    //     redb_api.set("set", lua.create_async_function(|_, (key, value): (String, u64)| async move {
+    //         write_db(key.as_str(), value).await.map_err(mlua::Error::external)?;
+    //         Ok(())
+    //     })?)?;
 
-        redb_api.set("get", lua.create_async_function(|_, key: String| async move {
-            let value = read_db(key.as_str()).await.map_err(mlua::Error::external)?;
-            Ok(value)
-        })?)?;
+    //     redb_api.set("get", lua.create_async_function(|_, key: String| async move {
+    //         let value = read_db(key.as_str()).await.map_err(mlua::Error::external)?;
+    //         Ok(value)
+    //     })?)?;
         
-        lua.globals().set("redb_api", redb_api)?;
-        Ok(())
-    }
+    //     lua.globals().set("redb_api", redb_api)?;
+    //     Ok(())
+    // }
 }
--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -2,3 +2,4 @@ pub mod engine;
 pub mod models;
 pub mod storage;
 pub mod rules;
+pub mod lua_api;
